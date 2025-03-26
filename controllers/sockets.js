941fe25c6a74b44b7b01679e83d28ceb88a6610c
David Castellanos Zayas <david383812@gmail.com>
2025-03-26 15:32:46 -0400

    test

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -3,8 +3,20 @@ const User = require("../models/User");
 const Ride = require("../models/Ride");
 const jwt = require("jsonwebtoken");
 const admin = require('firebase-admin');
-const path = require('path');
-const serviceAccount = require(path.resolve(__dirname, 'rapido.json'));
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN,
+};
+
 
 // Inicializa Firebase Admin con la cuenta de servicio
 admin.initializeApp({
